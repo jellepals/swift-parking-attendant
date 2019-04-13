7c3c69b1086c3e880418b6a60244ca53ace3b34b
Jelle Pals <jellepals@Jelles-MacBook-Pro.local>
2019-04-13 12:09:23 +0200

    First init new project

---
+++ han.parkingattendant/han.parkingattendant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  han.parkingattendant
+//
+//  Created by Jelle Pals on 4/13/19.
+//  Copyright © 2019 Jelle Pals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
